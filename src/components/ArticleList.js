38898b24133dcd4736cba8c8f009444cdef8c269
JDysiewicz <joshuadysiewicz@googlemail.com>
2020-10-29 13:35:18 +0000

    feat: Now corrently sorts posts to show the most recent first

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,12 +3,21 @@ import React from "react";
 import ArticleLink from "./ArticleLink";
 import usePosts from "../hooks/usePosts";
 
+// Move this out to a utils folder one day
+const recentToOld = (a, b) => {
+    let dateA = new Date(a.date);
+    let dateB = new Date(b.date);
+    return dateB - dateA;
+};
+
 export default () => {
     const posts = usePosts();
 
-    // Reverse to sort by most recent
+    // Sorts posts by most recent - making many Date objects is slow tho...
+    posts.sort((a,b) => recentToOld(a, b));
+
     const renderArticles = () => {
-        return posts.reverse().map(article => {
+        return posts.map(article => {
             return <ArticleLink key={article.slug} article={article} />
         });
     };
